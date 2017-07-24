d439e07b159f1000d81235447d7bb190b796412c
Vasiakozak1 <junglehunter2707@gmail.com>
2017-07-24 18:23:51 +0300

    Adding Advertisements.Web

---
+++ Advertisement.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Advertisement.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
