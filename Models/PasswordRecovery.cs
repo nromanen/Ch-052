24bac3fa189dbe53ecafb6622169702faa90acbc
Vasiakozak1 <junglehunter2707@gmail.com>
2017-07-24 16:48:28 +0300

    Adding new folder

--- Models/PasswordRecovery.cs
+++ Models/PasswordRecovery.cs
@@ -11,7 +11,7 @@ namespace AD.Models
         [Key, ForeignKey("User")]
         public int UserId { get; set; }
 
-        public string AccesHash { get; set; }
+        public string AccessHash { get; set; }
 
         public DateTime Expires { get; set; }
 
