a9f8e15edb27de486f202aa88f3b8e837e1def8c
Vasiakozak1 <junglehunter2707@gmail.com>
2017-07-25 16:22:40 +0300

    Add files via upload
    Adding DataAccess project.

---
+++ Advertisment.DataAccess/Migrations/201707251208202_Initial.cs
@@ -0,0 +1,17 @@
+namespace Advertisement.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+        
+        public override void Down()
+        { 
+        }
+    }
+}
---
+++ Advertisment.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Advertisement.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Advertisement.DataAccess.AdvertisementDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Advertisement.DataAccess.AdvertisementDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
