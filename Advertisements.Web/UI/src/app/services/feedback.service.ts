484fffc177cb5624042d1cecc180d9e92fe49e82
Марищук Дмитро <pidmogylnyi@gmail.com>
2017-08-17 18:19:36 +0300

    Post with token

--- Advertisements.Web/UI/src/app/services/feedback.service.ts
+++ Advertisements.Web/UI/src/app/services/feedback.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Feedback } from '../models/feedback';
-import {HttpModule, Http, Response, RequestOptions} from '@angular/http'; 
+import {Headers, HttpModule, Http, Response, RequestOptions} from '@angular/http'; 
 
 import { LoginService } from '../services/login.service';
 
@@ -25,8 +25,14 @@ constructor(private http: Http, private loginService: LoginService) { }
     } 
 
     postFeedback(param: any): Promise<any> {
+
+        let authToken = localStorage.getItem("access_token");
+        let headers = new Headers();
+        headers.append('Authorization', `Bearer ${authToken}`);
+        let options = new RequestOptions({ headers: headers });
+
     return this.http
-        .post('https://localhost:44384/api/feedback/add', param)
+        .post('https://localhost:44384/api/feedback/add', param, options)
         .toPromise()
         .then()
         .catch(this.handleError);
