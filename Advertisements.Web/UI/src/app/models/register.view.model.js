29bde87a854b68dcbc88ee5b46c731af2fc3bdfa
Vasiakozak1 <junglehunter2707@gmail.com>
2017-08-28 16:43:38 +0300

    Deleted unusable files

--- Advertisements.Web/UI/src/app/models/register.view.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class RegisterViewModel {
-}
-exports.RegisterViewModel = RegisterViewModel;
-//# sourceMappingURL=register.view.model.js.map
